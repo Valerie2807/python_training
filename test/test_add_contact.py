c7274c7b6e781075ae5a298016ebd80a2193fb91
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-11-15 17:55:06 +0300

    Выделили вспомогательные классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,13 +12,13 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Belka",
-                                        title="Blabla", company="Romashka", address="Prospect Mira, 12",
-                                        home="789812878",
-                                        mobile="7879878777", work="78789787987", fax="87987897987",
-                                        email="jhggjhg@gmail.com",
-                                        email2="ffgh@mail.ru", email3="yggh@yandex.ru", homepage="www.google.com",
-                                        bday="4",
-                                        bmonth="May", byear="1992", aday="5", amonth="February", ayear="2017",
-                                        address2="Peshkov street", phone2="849943434", notes="QA"))
+    app.contact.create(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Belka",
+                               title="Blabla", company="Romashka", address="Prospect Mira, 12",
+                               home="789812878",
+                               mobile="7879878777", work="78789787987", fax="87987897987",
+                               email="jhggjhg@gmail.com",
+                               email2="ffgh@mail.ru", email3="yggh@yandex.ru", homepage="www.google.com",
+                               bday="4",
+                               bmonth="May", byear="1992", aday="5", amonth="February", ayear="2017",
+                               address2="Peshkov street", phone2="849943434", notes="QA"))
     app.session.logout()
