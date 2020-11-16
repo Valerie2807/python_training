5ab42be8a65da1608c029e5c993a21f082a42e3a
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-11-16 23:16:19 +0300

    Оптимизация ввода данных в формы

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,6 +1,19 @@
+from model.group import Group
 
 
 def test_update_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.update_first_group()
+    app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New header"))
     app.session.logout()
