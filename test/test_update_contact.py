c91b1174cb2b34634440bb9c92d7091d5e3542f3
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-11-17 01:18:58 +0300

    Оптимизация ввода данных в формы Contact

--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -1,6 +1,13 @@
+from model.contact import Contact
 
 
-def test_update_first_contact(app):
+def test_modify_contact_name_company(app):
     app.session.login(username="admin", password="secret")
-    app.contact.update_first_contact()
+    app.contact.modify_first_contact(Contact(firstname="Elon", lastname="Mask", company="Google"))
+    app.session.logout()
+
+
+def test_modify_contact_address(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(address="Wroclaw, Poland"))
     app.session.logout()
--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,12 +1,6 @@
 from model.group import Group
 
 
-def test_update_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.update_first_group()
-    app.session.logout()
-
-
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
