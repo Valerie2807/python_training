110b2e256cacf7a325a69bbab1ca1b25f906ca1d
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-11-21 20:36:49 +0300

    Реализована проверка валидности фикстуры и проверка состояния сессии

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
-import pytest
 from fixture.application import Application
+import pytest
 
 fixture = None
 
@@ -9,18 +9,17 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
-    def final():
-        fixture.session.logout()
+    def fin():
+        fixture.session.ensure_logout()
         fixture.destroy()
-    request.addfinalizer(final)
+    request.addfinalizer(fin)
     return fixture
