e165c945584adc73132348628621dae3f3c29c25
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-12-21 01:28:52 +0300

    Неделя 7 Шаг 2 Работа с базами данных

---
+++ check_db_connection.py
@@ -0,0 +1,9 @@
+import pymysql
+connection = pymysql.Connect(host="127.0.0.1",database="addressbook", user="root", password="")
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
