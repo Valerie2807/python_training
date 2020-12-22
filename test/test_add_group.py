7c529d1c93cef74d0032a1cbbecccc94f6987f06
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-12-22 17:11:56 +0300

    Неделя 7 Шаг 4 Переделываем тесты: проверяем данные, загруженные из базы данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,12 +2,12 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
   group = json_groups
-  old_groups = app.group.get_group_list()
+  old_groups = db.get_group_list()
   group = Group(name="TestGroup", header="header", footer="footer")
   app.group.create(group)
-  new_groups = app.group.get_group_list()
+  new_groups = db.get_group_list()
   old_groups.append(group)
   assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
